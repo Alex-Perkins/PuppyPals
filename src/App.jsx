6f9d9899a94c960a1c609881c6497ee7fdc00fdf
AlexPerkins807 <kokookachualex@gmail.com>
2025-02-19 16:37:19 -0600

    updated 2/19

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//Import puppyList from the data.js file
+import { puppyList } from './data.js'
 
-      <div>
-      
-      </div>
+//Import the useState hook
+import { useState } from 'react'
+
+//import a CSS file
+import './index.css'
+
+function App() {
+
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  function handleClick() {
+
+  }
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+
+console.log(featuredPup);
+
+  return (
+
+<div className="appContainer">
+
+    <div className="App">
+
+      {puppies.map((puppy) => {
+
+        return ( <p onClick={ () => { setFeatPupId(puppy.id) } }  key={puppy.id}> {puppy.name} </p>
+
+        );
+
+      })}
+
+{featPupId && (
+        <div>
+          <h2> {featuredPup.name} </h2>
+          <ul>
+            <li> Age: {featuredPup.age} </li>
+            <li> Email: {featuredPup.email} </li>
+          </ul>
+        </div>
+      )}
+
+    </div>
+
+</div>
+
+  );
+
+};
+
+export default App
+
     
 
 
